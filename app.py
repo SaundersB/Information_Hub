031fd081e48f76912858539123227cae4c185955
SaundersB <speakeasys@gmail.com>
2016-08-21 18:10:33 +0000

    Adding the date/time.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import time
+from datetime import datetime
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def information():
+	current_time = format = time.strftime("%a %b %d, %Y  %I:%M:%S %p",time.localtime(time.time()))
+	
+
+	current_military_time = time.asctime( time.localtime(time.time()) )
+	return render_template('index.html', clock_time=current_time)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
